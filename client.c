a74e93635160ee99b77a971296c9dd0301ca08df
RoadoneP <a01152a@gmail.com>
2021-04-21 02:21:15 +0900

    Success!

--- client.c
+++ client.c
@@ -8,26 +8,26 @@
 
 #define BUFFER_SIZE 1460
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int sockfd = 0;
     struct sockaddr_in serv_addr;
 
     int str_len;
-    char message[20] = "Hello Server!";
+    char message[20]="Hello Server!";
     char filename[20];
 
     char command;
 
     // If not exist IP and Port num
-    if (argc != 3)
+    if(argc != 3)
     {
-        printf("\n Usage: %s <ip of server> <port> \n", argv[0]);
+        printf("\n Usage: %s <ip of server> <port> \n",argv[0]);
         return 1;
     }
+  
 
-
-    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
         return 1;
@@ -36,23 +36,23 @@ int main(int argc, char* argv[])
     memset(&serv_addr, '0', sizeof(serv_addr)); // 0?
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
 
-    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        printf("\n Error : Connect Failed \n");
-        return 1;
+       printf("\n Error : Connect Failed \n");
+       return 1;
     }
 
     printf("\nSending \"%s\" to Client..\n", message);
-    write(sockfd, message, strlen(message));
+    write(sockfd,message,strlen(message));
 
-    str_len = read(sockfd, message, strlen(message));
-    if (str_len < 0) {
-        printf("\n Error: read() error");
-        return 1;
+    str_len=read(sockfd, message, strlen(message));
+    if(str_len<0){
+      printf("\n Error: read() error");
+      return 1;
     }
 
     printf("Server->Client : %s\n", message);
@@ -60,43 +60,53 @@ int main(int argc, char* argv[])
     int file_size;
     char read_message[50];
     int bytes_read;
-    char buff[BUFFER_SIZE] = { 0, };
-    int total = 0;
+    char buff[BUFFER_SIZE]={0,};
+    int total=0;
+    char str_file_size[15];
+    int index=0;
 
-    read(sockfd, read_message, sizeof(read_message));
+    read(sockfd, read_message,sizeof(read_message));
     printf("\n%s [Y/n]\n", read_message);
 
     scanf("%c", &command);
     write(sockfd, &command, sizeof(command));
 
-    if (command == 'y' || command == 'Y') {
-        FILE* fp = fopen("test.mp4", "wb");
-        if (fp == NULL)
-        {
-            printf("\n Error : File is Null!\n");
-            return 1;
+    read(sockfd, str_file_size, 15);
+    file_size = atoi(str_file_size);
+
+    if(command =='y'|| command =='Y'){
+      FILE *fp = fopen("rece.mp4","wb");
+      if(fp==NULL)
+      {
+        printf("\n Error : File is Null!\n");
+        return 1;
+      }
+
+      printf("Download in progress\n");
+
+      while((bytes_read=read(sockfd,buff,BUFFER_SIZE))>0){
+        total+=bytes_read;
+        if(bytes_read<0){
+          printf("Error : Receiving Error");
+          return 1;
         }
 
-        printf("Download in progress\n");
-
-        while ((bytes_read = read(sockfd, buff, BUFFER_SIZE)) > 0) {
-            total += bytes_read;
-            if (bytes_read < 0) {
-                printf("Error : Receiving Error");
-                return 1;
-            }
-
-            if (fwrite(buff, sizeof(char), bytes_read, fp) < 0) {
-                printf("Error : File Write Error");
-                return 1;
-            }
-
-            memset(buff, 0, BUFFER_SIZE);
+        if(fwrite(buff, sizeof(char), bytes_read, fp)<0){
+          printf("Error : File Write Error");
+          return 1;
         }
 
-        printf("Receive Success, %d bytes received.\n", total);
+        ++index;
+        if(index % 100 == 0){
+          printf("Progressing : %d / %d \n",total, file_size);
+        }
 
-        fclose(fp);
+        memset(buff,0, BUFFER_SIZE);
+      }
+
+      printf("Receive Success, %d bytes received.\n", total);
+
+      fclose(fp);
     }
 
     close(sockfd);
--- server.c
+++ server.c
@@ -7,137 +7,152 @@
 #include <sys/types.h>
 
 // 1460bytes
-#define BUFFER_SIZE 14600
+#define BUFFER_SIZE 1460
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // fd socket
-    int listenfd = 0, connfd = 0;
+        // fd socket
+        int listenfd = 0, connfd = 0;
 
-    // It contains the socket address.
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
-    char command;
-    char send_message[] = "Would you like to send the file? : test.mp4";
+        // It contains the socket address.
+        struct sockaddr_in serv_addr; 
+        struct sockaddr_in clnt_addr; 
+        char command;
+        char send_message[] = "Would you like to send the file? : test.mp4";
 
-    int str_len;
+        int str_len;
 
-    //Client address size
-    socklen_t clnt_addr_size;
+        //Client address size
+        socklen_t clnt_addr_size;
 
-    char message[20];
-    char filename[20];
-
-
-    // When the port is not input
-    if (argc != 2)
-    {
-        printf("\n Usage: %s <ip of server> \n", argv[0]);
-        return 1;
-    }
+        char message[20];
+        char filename[20];
 
 
-    //PF=> socket, AF=> sockaddr_in   Socket creation
-    if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    }
-
-    //reset(address)
-    memset(&serv_addr, '0', sizeof(serv_addr));
-
-    //IPv4 address system
-    //È£½ºÆ®-³×Æ®¿öÅ©(longÅ¸ÀÔ: ÁÖ¼Ò), INADDR_ANY : IPÁÖ¼Ò ÀÚµ¿ ÇÒ´ç
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    //È£½ºÆ®-³×Æ®¿öÅ©(shortÅ¸ÀÔ:Æ÷Æ®), ¸í·É¾î ÀÔ·Â ½Ã Ã¹¹øÂ° ÀÎÀÚ°¡ Æ÷Æ® ¹øÈ£·Î ÀÌµ¿
-    serv_addr.sin_port = htons((atoi(argv[1])));
-
-    //Address assignment by bind
-    if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-    {
-        printf("\n Error : bind() error\n");
-        return 1;
-    }
-
-    if (listen(listenfd, 5) < 0) {
-        printf("\n Error: listen() error\n");
-    }
-
-    //Address size
-    clnt_addr_size = sizeof(clnt_addr);
-
-    // Accept()
-    connfd = accept(listenfd, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-    if (connfd < 0) {
-        printf("\n Error : accept() error\n");
-        return 1;
-    }
-
-    // Read message
-    str_len = read(connfd, message, sizeof(message));
-    if (str_len < 0) {
-        printf("\n Error : read() error\n");
-        return 1;
-    }
-    printf("Client->Server : %s\n", message);
-    //
-
-    // Message send
-    strcpy(message, "Hello Client!");
-
-    printf("Sending \"%s\" to Client..\n", message);
-    write(connfd, message, strlen(message));
-
-    //--------------------------File Transfer---------------------------------
-    int command_len;
-    int bytes_read;
-    char buff[BUFFER_SIZE] = { 0 };
-    int total = 0;
-
-    write(connfd, send_message, strlen(send_message));
-
-    command_len = read(connfd, &command, sizeof(command));
-
-    if (command_len < 0) {
-        printf("\n Error : read() error\n");
-        return 1;
-    }
-
-    if (command == 'y' || command == 'Y') {
-        FILE* fp = fopen("test.mp4", "rb");
-        if (fp == NULL)
+        // When the port is not input
+        if(argc != 2)
         {
-            printf("\n Error : File is Null!\n");
-            return 1;
+          printf("\n Usage: %s <ip of server> \n",argv[0]);
+          return 1;
         }
 
-        printf("Sending in progress\n");
 
-        while (!feof(fp)) {
-            bytes_read = fread(buff, sizeof(char), BUFFER_SIZE, fp);
-            printf("%d\n", bytes_read);
-            total += bytes_read;
-            printf("connect");
-            if (bytes_read != BUFFER_SIZE && ferror(fp)) {
-                printf("\n Error : File Read error\n");
-                return 1;
+        //PF=> socket, AF=> sockaddr_in   Socket creation
+        if((listenfd = socket(PF_INET, SOCK_STREAM, 0))<0){
+          printf("\n Error : Could not create socket \n");
+          return 1;
+        }
+
+        //reset(address)
+        memset(&serv_addr, '0', sizeof(serv_addr));
+        
+        //IPv4 address system
+        //í˜¸ìŠ¤íŠ¸-ë„¤íŠ¸ì›Œí¬(longíƒ€ìž…: ì£¼ì†Œ), INADDR_ANY : IPì£¼ì†Œ ìžë™ í• ë‹¹
+        serv_addr.sin_family = AF_INET; 
+        serv_addr.sin_addr.s_addr= htonl(INADDR_ANY);
+        //í˜¸ìŠ¤íŠ¸-ë„¤íŠ¸ì›Œí¬(shortíƒ€ìž…:í¬íŠ¸), ëª…ë ¹ì–´ ìž…ë ¥ ì‹œ ì²«ë²ˆì§¸ ì¸ìžê°€ í¬íŠ¸ ë²ˆí˜¸ë¡œ ì´ë™
+        serv_addr.sin_port = htons((atoi(argv[1])));
+
+        //Address assignment by bind
+        if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+        {
+          printf("\n Error : bind() error\n");
+          return 1;
+        }
+
+        if(listen(listenfd, 5) < 0){
+          printf("\n Error: listen() error\n");
+        }
+
+        //Address size
+        clnt_addr_size=sizeof(clnt_addr);
+        
+        // Accept()
+        connfd=accept(listenfd, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+        if(connfd<0){
+          printf("\n Error : accept() error\n");
+          return 1;
+        }
+
+        // Read message
+        str_len = read(connfd, message, sizeof(message));
+        if(str_len<0){
+          printf("\n Error : read() error\n");
+          return 1;
+        }
+        printf("Client->Server : %s\n", message);
+        //
+
+        // Message send
+        strcpy(message, "Hello Client!");
+
+        printf("Sending \"%s\" to Client..\n", message);
+        write(connfd, message, strlen(message));
+        
+        //--------------------------File Transfer---------------------------------
+        int command_len;
+        int bytes_read;
+        char buff[BUFFER_SIZE]={0,};
+        int total=0;
+        char str_file_size[15];
+        FILE *checkFile;
+        int file_size=0;
+        int index=0;
+
+        write(connfd, send_message,strlen(send_message));
+
+        command_len = read(connfd, &command, sizeof(command));
+
+        checkFile = fopen("test.mp4","rb");
+        if (checkFile == NULL){
+          printf("\n Error : File is Null!\n");
+          return 1;
+        }
+
+        fseek(checkFile, 0, SEEK_END);
+        file_size = ftell(checkFile);
+
+        printf("Total File Size : %d\n", file_size);
+        sprintf(str_file_size, "%d", file_size);  // integer -> string
+        write(connfd, str_file_size, strlen(str_file_size));
+        printf("Send File Size to Client!\n");
+        fclose(checkFile);
+
+        if(command_len<0){
+          printf("\n Error : read() error\n");
+          return 1;
+        }
+
+        if(command == 'y' || command == 'Y'){
+          FILE *fp = fopen("test.mp4","rb");
+
+          printf("Sending in progress\n");
+          
+          while((bytes_read=fread(buff, sizeof(char),BUFFER_SIZE, fp))>0){
+            total+=bytes_read;
+            if(bytes_read!=BUFFER_SIZE&&ferror(fp)){
+              printf("\n Error : File Read error\n");
+              return 1;
             }
 
-            if (write(connfd, buff, bytes_read) < 0) {
-                printf("\n Error : Send Error\n");
-                return 1;
+            if(write(connfd, buff, bytes_read) < 0){
+              printf("\n Error : Send Error\n");
+              return 1;
             }
+
+            ++index;
+            if (index % 100 == 0)
+              printf("Progressing : %d / %d\n", total, file_size);
 
             memset(buff, 0, BUFFER_SIZE);
+          }
+          printf("Send Success, %d bytes send\n", total);
+
+          fclose(fp);
         }
-        printf("Send Success, %d bytes send\n", total);
 
-        fclose(fp);
-    }
+        close(connfd);
+        close(listenfd);
 
-    close(connfd);
-    close(listenfd);
-
-    return 0;
+        return 0;
 }
